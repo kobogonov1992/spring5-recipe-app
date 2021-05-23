55b7a0750979cabde17495e31421a49f64ba3bd9
adiletkobogonov <kobogonov1992@gmail.com>
2021-05-23 15:15:34 +0600

    Adding Data repositories

---
+++ src/main/java/com/example/spring5recipeapp/repositories/RecipeRepository.java
@@ -0,0 +1,10 @@
+package com.example.spring5recipeapp.repositories;
+
+import com.example.spring5recipeapp.models.Recipe;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RecipeRepository extends CrudRepository <Recipe,Long> {
+
+
+
+}
